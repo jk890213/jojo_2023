0aa7e02f83eb2082b6ce9f486be3f0aacc6ae193
HORIZON\joseph.li <joseph.li@hzn.com.tw>
2023-06-09 11:41:43 +0800

    feat(rwd): nav 響應式畫面

--- src/views/home/components/VideoBanner.vue
+++ src/views/home/components/VideoBanner.vue
@@ -9,9 +9,17 @@
         </li>
       </ul>
     </nav>
-    <!-- <nav aria-label="mobile_nav">
-      <RouterLink to="/">Home</RouterLink>
-    </nav> -->
+    <nav aria-label="mobile_nav">
+      <div class="menu_burger" @click="toggleMenu">
+        <div />
+      </div>
+      <ul :class="{ open: isOpenMenu }">
+        <li v-for="{ routeName, title } in nav" :key="routeName">
+          <RouterLink to="/">{{ title }}</RouterLink>
+        </li>
+      </ul>
+      <div class="logo">JOJO</div>
+    </nav>
     <p>
       <span>仿製</span>
       <span>MOMO 壽喜燒</span>
@@ -34,6 +42,11 @@ const props = defineProps({
     required: true
   }
 })
+
+const isOpenMenu = ref(false)
+const toggleMenu = () => {
+  isOpenMenu.value = !isOpenMenu.value
+}
 
 const isScrolledPastViewport = ref(false)
 const navHeight = 56
@@ -80,6 +93,10 @@ header {
     z-index: 10;
     background-color: rgba(193, 39, 45, 0.9);
 
+    @include desktops {
+      display: none;
+    }
+
     .logo {
       line-height: 40px;
       font-size: 40px;
@@ -102,7 +119,90 @@ header {
     }
   }
 
-  nav[aria-label='pc_nav'].top {
+  nav[aria-label='mobile_nav'] {
+    display: none;
+    width: 100%;
+    height: 80px;
+    line-height: 80px;
+    z-index: 10;
+    background-color: #fff;
+
+    @include desktops {
+      display: flex;
+    }
+
+    .logo {
+      text-align: center;
+      color: $majorColor;
+      flex-grow: 1;
+    }
+
+    .menu_burger {
+      width: 40px;
+      margin: 20px 0 20px 20px;
+      position: relative;
+      // background-color: #aca;
+
+      &::before {
+        content: '';
+        position: absolute;
+        top: 10px;
+        left: 5px;
+        right: 5px;
+        height: 5px;
+        background-color: $majorColor;
+        border-radius: 4px;
+      }
+
+      &::after {
+        content: '';
+        position: absolute;
+        top: 30px;
+        left: 5px;
+        right: 5px;
+        height: 5px;
+        background-color: $majorColor;
+        border-radius: 4px;
+      }
+
+      div {
+        position: absolute;
+        top: 20px;
+        left: 5px;
+        right: 5px;
+        height: 5px;
+        background-color: $majorColor;
+        border-radius: 4px;
+      }
+    }
+
+    ul {
+      position: absolute;
+      top: 80px;
+      height: 0;
+      width: 100%;
+      overflow: hidden;
+      background-color: #efefef;
+      transition: height 0.9s;
+
+      li {
+        border-top: 1px solid #fff;
+        text-align: center;
+
+        a {
+          text-decoration: none;
+          color: #898989;
+        }
+      }
+    }
+
+    ul.open {
+      height: 100vh;
+    }
+  }
+
+  nav[aria-label='pc_nav'].top,
+  nav[aria-label='mobile_nav'] {
     position: fixed;
     top: 0;
   }
